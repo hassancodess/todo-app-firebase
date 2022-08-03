d43c6fe2f1c8249ec15f1661ae5567ffacbae66f
hassancodess <dev.hassancodes@gmail.com>
2022-08-03 10:05:20 +0500

    Add Animations

--- src/components/todos/TodoList.jsx
+++ src/components/todos/TodoList.jsx
@@ -3,20 +3,26 @@ import TodoItem from './TodoItem'
 import { AnimatePresence, motion } from 'framer-motion'
 function TodoList({ todos, toggleComplete, deleteTodo }) {
   return (
-    <div>
+    <AnimatePresence>
       <div className='flex flex-col justify-center items-center space-y-3  '>
-        {todos.map((todo, index) => {
-          return (
+        {todos.map((todo, index) => (
+          <motion.div
+            className='w-full items-center flex flex-col'
+            key={index}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
             <TodoItem
               key={index}
               todo={todo}
               toggleComplete={toggleComplete}
               deleteTodo={deleteTodo}
             />
-          )
-        })}
+          </motion.div>
+        ))}
       </div>
-    </div>
+    </AnimatePresence>
   )
 }
 
