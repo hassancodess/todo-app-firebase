ba852d296a687299d4870aefd40c540cc85037a5
hassancodess <dev.hassancodes@gmail.com>
2022-08-03 10:30:52 +0500

    Add API keys to env

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,21 @@ import { initializeApp } from 'firebase/app'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyAeUmS4_Br5poGBP5mOHP4v2qN9956eHnc',
+//   authDomain: 'todo-firebase-22383.firebaseapp.com',
+//   projectId: 'todo-firebase-22383',
+//   storageBucket: 'todo-firebase-22383.appspot.com',
+//   messagingSenderId: '180621548863',
+//   appId: '1:180621548863:web:b46b2a5c40f125fdb4878a',
+// }
 const firebaseConfig = {
-  apiKey: 'AIzaSyAeUmS4_Br5poGBP5mOHP4v2qN9956eHnc',
-  authDomain: 'todo-firebase-22383.firebaseapp.com',
-  projectId: 'todo-firebase-22383',
-  storageBucket: 'todo-firebase-22383.appspot.com',
-  messagingSenderId: '180621548863',
-  appId: '1:180621548863:web:b46b2a5c40f125fdb4878a',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 }
 
 // Initialize Firebase
